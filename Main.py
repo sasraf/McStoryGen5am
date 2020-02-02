adb22d85152c08e1cdaaaa309c5805de69541a1c
Alex <alexander.lu2@mail.mcgill.ca>
2020-02-02 07:11:42 -0500

    See comment

--- Main.py
+++ Main.py
@@ -5,3 +5,4 @@ textgen = textgenrnn(weights_path='Test3_weights.hdf5',
 
 textgen.generate_samples(max_gen_length=1000)
 textgen.generate_to_file('textgenrnn_texts.txt', max_gen_length=1000)
+#let's try this
