bcc691bdd5e926f04a60ac7a96d1e0d0991236a1
Alex <alexander.lu2@mail.mcgill.ca>
2020-02-02 07:10:31 -0500

    Initial commit

---
+++ Main.py
@@ -0,0 +1,7 @@
+from textgenrnn import textgenrnn
+textgen = textgenrnn(weights_path='Test3_weights.hdf5',
+                       vocab_path='Test3_vocab.json',
+                       config_path='Test3_config.json')
+
+textgen.generate_samples(max_gen_length=1000)
+textgen.generate_to_file('textgenrnn_texts.txt', max_gen_length=1000)
